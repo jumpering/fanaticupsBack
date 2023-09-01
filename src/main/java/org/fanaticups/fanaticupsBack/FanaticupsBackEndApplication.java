6dc32f2fb6f346bcba557f61970b108f014db2f7
xavi gomez <jumpering@gmail.com>
2023-09-02 01:02:52 +0200

    first commit - project created

---
+++ src/main/java/org/fanaticups/fanaticupsBack/FanaticupsBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.fanaticups.fanaticupsBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanaticupsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FanaticupsBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fanaticups/fanaticupsBack/FanaticupsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fanaticups.fanaticupsBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanaticupsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
