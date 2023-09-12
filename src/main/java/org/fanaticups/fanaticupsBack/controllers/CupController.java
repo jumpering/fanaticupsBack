e578391f8c69f8eb47a93b30490229c20c89c369
xavi gomez <jumpering@gmail.com>
2023-09-12 16:41:09 +0200

    add OpenAPI and Swagger

--- src/main/java/org/fanaticups/fanaticupsBack/controllers/CupController.java
+++ src/main/java/org/fanaticups/fanaticupsBack/controllers/CupController.java
@@ -12,12 +12,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 public class CupController {
 
     @Autowired
     private CupService cupService;
     
+    @Operation(summary = "Get all cups")
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", 
+                        description = "Found cups", 
+                        content = { 
+                                @Content(mediaType = "application/json", 
+                                    schema = @Schema(implementation = CupDTO.class))}
+                                    )})
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = "/cups")
     public ResponseEntity<List<CupDTO>> findAllCups(){
