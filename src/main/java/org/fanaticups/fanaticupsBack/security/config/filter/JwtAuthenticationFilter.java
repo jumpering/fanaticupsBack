f158eed168e957a143296e737bf9c340d68dd771
jumpering <jumpering@gmail.com>
2023-11-05 10:11:14 +0100

    refactor

--- src/main/java/org/fanaticups/fanaticupsBack/security/config/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/fanaticups/fanaticupsBack/security/config/filter/JwtAuthenticationFilter.java
@@ -8,6 +8,7 @@ import org.fanaticups.fanaticupsBack.security.services.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -18,7 +19,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
     private UserRepository userRepository;
@@ -29,42 +30,61 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
-        //1. obtener el header con el jwt
-        String authHeader = request.getHeader("Authorization"); //Bearer
-        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+
+        // 1. obtener el header con el jwt
+        String authHeader = request.getHeader("Authorization"); // Bearer
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
-        //2 Obtener el jwt del header
+        // 2 Obtener el jwt del header
         String jwt = authHeader.split(" ")[1];
 
-        //3. Obtener el subject/usename desde el jwt
+        // 3. Obtener el subject/usename desde el jwt
         String username = this.jwtService.extractUsername(jwt);
 
-        //4. Setear un objeto Authentication dentro del securityContext (que está dentro del securityContextHolder)
-        
+        // 4. Setear un objeto Authentication dentro del securityContext (que está
+        // dentro del securityContextHolder)
+
         UserEntity userEntity = this.userRepository.findByEmail(username).get();
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-            username, null, userEntity.getAuthorities()
-        );
+                username, null, userEntity.getAuthorities());
 
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
-        //5. ejecutar el resto de filres
+        // 5. ejecutar el resto de filres
         filterChain.doFilter(request, response);
 
-
-
-
-        //////....TODO para agregar header s nuevas peticiones
+        ////// ....TODO para agregar header s nuevas peticiones
         // if (jwtUtilService.validateToken(jwt, userDetails)) {
+
+        // UsernamePasswordAuthenticationToken authenticationToken = new
+        // UsernamePasswordAuthenticationToken(
+        // userDetails, null, userDetails.getAuthorities());
+        // authenticationToken.setDetails(new
+        // WebAuthenticationDetailsSource().buildDetails(request));
+        // SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        // }
+
+
+
+
+        
+
+        // if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+        //     UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+        //     if (jwtUtilService.validateToken(jwt, userDetails)) {
 
         //         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
         //                 userDetails, null, userDetails.getAuthorities());
         //         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         //         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         //     }
+        // }
+        // chain.doFilter(request, response);
+
     }
-    
+
 }
