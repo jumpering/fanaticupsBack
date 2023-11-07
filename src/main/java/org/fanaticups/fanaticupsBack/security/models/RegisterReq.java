04b35d3a9c407c6c0e5db2e9b00419659b6b0586
jumpering <jumpering@gmail.com>
2023-11-07 21:09:52 +0100

    refactor

---
+++ src/main/java/org/fanaticups/fanaticupsBack/security/models/RegisterReq.java
@@ -0,0 +1,16 @@
+package org.fanaticups.fanaticupsBack.security.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterReq {
+
+    private String name;
+    private String password;
+    private String email;
+    
+}
