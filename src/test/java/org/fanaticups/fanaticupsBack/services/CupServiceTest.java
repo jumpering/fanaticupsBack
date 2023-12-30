442ab843e15b165c245e303e7cba122546b55a20
jumpering <jumpering@gmail.com>
2023-12-30 10:43:20 +0100

    wip testing services

--- src/test/java/org/fanaticups/fanaticupsBack/services/CupServiceTest.java
+++ src/test/java/org/fanaticups/fanaticupsBack/services/CupServiceTest.java
@@ -18,16 +18,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(MockitoExtension.class)
 class CupServiceTest {
     @Mock
     private CupRepository cupRepository;
     @InjectMocks
     private CupService cupService;
-    private CupDTO cupOne;
-    private CupEntity cupOneEntity;
+    private CupDTO cupDTO;
+    private CupEntity cupEntity;
     private UserEntity userEntity;
     private String imagePath;
     @BeforeEach
@@ -37,29 +35,29 @@ class CupServiceTest {
                 .name("xavi")
                 .email("x@x.x")
                 .build();
-        this.cupOne = CupDTO.builder()
+        this.cupDTO = CupDTO.builder()
                 .id(1L)
                 .name("Cup1")
                 .Description("Description")
                 .image("")
                 .user(this.userEntity).build();
-        this.cupOneEntity = CupEntity.builder().id(1L)
+        this.cupEntity = CupEntity.builder().id(1L)
                 .name("Cup1")
                 .description("Description")
                 .image("image.jpg")
                 .user(this.userEntity)
                 .build();
-        this.imagePath = this.userEntity.getId() + "/" + this.cupOneEntity.getName() + "/";
+        this.imagePath = this.userEntity.getId() + "/" + this.cupEntity.getName() + "/";
     }
     @DisplayName("ServiceCup method findAllCups()")
     @Test
     public void givenRequestAllCupsWhenFindAllCupsInServiceThenReturnAllCups(){
         List<CupEntity> cupListEntityForMock = new ArrayList<>();
-        cupListEntityForMock.add(this.cupOneEntity);
+        cupListEntityForMock.add(this.cupEntity);
         Mockito.when(this.cupRepository.findAll()).thenReturn(cupListEntityForMock);
         List<CupDTO> cupListExpected = new ArrayList<>();
-        this.cupOne.setImage(this.imagePath + "image.jpg");
-        cupListExpected.add(this.cupOne);
+        this.cupDTO.setImage(this.imagePath + "image.jpg");
+        cupListExpected.add(this.cupDTO);
         List<CupDTO> result = this.cupService.findAllCups();
         Assertions.assertEquals(cupListExpected, result);
     }
@@ -67,9 +65,9 @@ class CupServiceTest {
     @DisplayName("ServiceCup method findCupById existing id")
     @Test
     public void givenRequestExistOneCupWhenFindByIdThenReturnOptionalCup(){
-        Mockito.when(this.cupRepository.findById(1L)).thenReturn(Optional.ofNullable(this.cupOneEntity));
-        this.cupOne.setImage(this.imagePath + "image.jpg");
-        CupDTO cupDTOExpected = this.cupOne;
+        Mockito.when(this.cupRepository.findById(1L)).thenReturn(Optional.ofNullable(this.cupEntity));
+        this.cupDTO.setImage(this.imagePath + "image.jpg");
+        CupDTO cupDTOExpected = this.cupDTO;
         Assertions.assertEquals(Optional.of(cupDTOExpected), this.cupService.findCupById(1L));
     }
 
@@ -77,8 +75,12 @@ class CupServiceTest {
     @Test
     public void givenRequestNoExistOneCupWhenFindByIdThenReturnOptionalEmpty(){
         Mockito.when(this.cupRepository.findById(2L)).thenReturn(Optional.empty());
-        this.cupOne.setImage(this.imagePath + "image.jpg");
         Assertions.assertEquals(Optional.empty(), this.cupService.findCupById(2L));
+    }
+    @DisplayName("ServiceCUp method add cup then return the new cup")
+    @Test
+    public void givenCupWhenAddCupServiceThenReturnCupCreated(){
+        Mockito.when(this.cupRepository.save(this.cupEntity)).thenReturn(this.cupEntity);
     }
 
 }
