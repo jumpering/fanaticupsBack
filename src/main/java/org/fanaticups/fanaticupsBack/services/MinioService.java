170e2d607eedcce359fef3501caf9c07fd9f2811
jumpering <jumpering@gmail.com>
2024-02-22 20:21:51 +0100

    change routes minioService

--- src/main/java/org/fanaticups/fanaticupsBack/services/MinioService.java
+++ src/main/java/org/fanaticups/fanaticupsBack/services/MinioService.java
@@ -2,29 +2,41 @@ package org.fanaticups.fanaticupsBack.services;
 
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.errors.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @Service
 public class MinioService {
 
     private MinioClient minioClient;
+    //private final String accesKeyId = "Bearer DjjGWp1QpWL87Q9Z7fJT";
     private final String accesKeyId = "DjjGWp1QpWL87Q9Z7fJT";
     private final String secretAccessKey = "BaoeKiv67fBHJmwJan5se7NXcLyImYeAUeMRNKWY";
-    private final String apiMinioUrl = "http://172.17.0.5:9000";
+    private final String apiMinioUrl = "http://172.17.0.5:9000"; //ESTE FUNCIONA!!!!!!!!!
+    //private final String apiMinioUrl = "//images";
     private final String baseImagePath = "/fanaticups/";
     private final String bucketName = "images";
 
     //public MinioService(@Value("${apiBaseUrlMinio}") String apiMinioUrl) {
     public MinioService() {
         //this.apiMinioUrl = apiMinioUrl;
-        //this.apiMinioUrl = "http://172.17.0.5:9000";
+        //this.apiMinioUrl = "172.17.0.5:9000";
         try {
             this.minioClient = MinioClient.builder()
                     .endpoint(this.apiMinioUrl)
                     .credentials(this.accesKeyId, this.secretAccessKey)
                     .build();
+            System.out.println("minio registrado!!! " + this.minioClient);
         } catch (Exception e) {
             System.out.println("Error on authenticate minio and init: " + e);
         }
