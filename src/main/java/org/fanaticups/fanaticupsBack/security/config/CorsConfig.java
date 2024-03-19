cbe6d91608f0ac17c35b7e17b911999e243afc9c
jumpering <jumpering@gmail.com>
2024-03-19 21:09:49 +0100

    enable security JWT

--- src/main/java/org/fanaticups/fanaticupsBack/security/config/CorsConfig.java
+++ src/main/java/org/fanaticups/fanaticupsBack/security/config/CorsConfig.java
@@ -1,27 +1,12 @@
 package org.fanaticups.fanaticupsBack.security.config;
 
-import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
-  // @Bean
-  // CorsConfigurationSource corsConfigurationSource() {
-  //   CorsConfiguration configuration = new CorsConfiguration();
-  //   configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-  //   configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
-  //   UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-  //   source.registerCorsConfiguration("/**", configuration);
-  //   return source;
-  // }
-
 
 @Bean
   public WebMvcConfigurer corsConfigurer(){
@@ -31,33 +16,9 @@ public class CorsConfig {
       public void addCorsMappings(CorsRegistry registry){
 
         registry.addMapping("/**")
-        //.allowedOrigins("http://localhost:4200")
-
         .allowedOrigins("http://fanaticups.org, https://fanaticups.org, http://www.fanaticups.org, https://www.fanaticups.org, http://5.250.184.31:4200, http://5.250.184.31, https://fanaticups.onrender.com/files, http://localhost:4200")
         .allowedMethods("*")
         .exposedHeaders("*");
-
-
-
-        // registry.addMapping("/authenticate")
-        // .allowedOrigins("http://localhost:4200")
-        // .allowedMethods("*")
-        // .exposedHeaders("*");
-
-
-        // registry.addMapping("/test")
-        // .allowedOrigins("http://localhost:4200")
-        // .allowedMethods("*")
-        // .exposedHeaders("*");
-
-
-        // registry.addMapping("/cups")
-        // .allowedOrigins("http://localhost:4200")
-        // .allowedMethods("*");
-
-        // registry.addMapping("/")
-        // .allowedOrigins("http://localhost:4200")
-        // .allowedMethods("*");
       }
     };
   }
--- src/main/java/org/fanaticups/fanaticupsBack/security/config/WebSecurityConfig.java
+++ src/main/java/org/fanaticups/fanaticupsBack/security/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package org.fanaticups.fanaticupsBack.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.StaticResourceLocation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,10 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toStaticResources;
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.fanaticups.fanaticupsBack.security.filters.JWTAuthenticationFilter;
 
 @Configuration
@@ -34,44 +31,23 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable) //to avoid 403 forbiden error on post
                 //.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> authorize
-//                                .requestMatchers("/authenticate/**").permitAll()
-//                                .requestMatchers("/register/**").permitAll()
-//                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-//                                .requestMatchers(HttpMethod.GET, "/cups").permitAll()
-//                                .requestMatchers(HttpMethod.POST,"/cups/**").authenticated()
-//                                .requestMatchers(HttpMethod.GET, "/cups/{id}").permitAll()
-//                                .requestMatchers(HttpMethod.PUT,"/cups/{id}").authenticated()
-//                                .requestMatchers(HttpMethod.DELETE,"/cups/{id}").authenticated()
-//                                .requestMatchers(toStaticResources().atCommonLocations()).permitAll()
-//                                .requestMatchers(HttpMethod.GET, "/files/**").permitAll()
-//                                .requestMatchers(HttpMethod.POST, "/files/**").authenticated()
-//                                .requestMatchers(HttpMethod.GET, "/images/**").permitAll()
-//                                .requestMatchers(HttpMethod.GET, "file:/Users/xaviergomezcanals/Documents/Projects/fanaticups/images/**").permitAll()
-//                                .requestMatchers(HttpMethod.GET,"/resources/**").permitAll()
-//                                .anyRequest().authenticated()
-                                .anyRequest().permitAll()
+                                .requestMatchers("/api/authenticate/**").permitAll()
+                                //.requestMatchers("/api/register/**").permitAll()
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/cups/**").permitAll()
+                                .requestMatchers(HttpMethod.POST,"/api/cups/**").authenticated()
+                                .requestMatchers(HttpMethod.PUT,"/api/cups/**").authenticated()
+                                .requestMatchers(HttpMethod.DELETE,"/api/cups/**").authenticated()
+                                .anyRequest().authenticated()
+//                                .anyRequest().permitAll()
                                 //.requestMatchers("/authenticate/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").permiteAll()
-
-
                 )
                 .addFilterBefore(JWTAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
-
-        // http
-        //     .formLogin(withDefaults()); // (1)
-        // http
-        //     .httpBasic(withDefaults()); // (1)
-
         return http.build();
     }
-
-//    @Bean
-//    PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-
 
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
